13807eb6d158a2c8816d23f363f53081b651e4c3
Srinath <srinath001me@gmail.com>
2022-09-29 13:08:22 +0530

    notification setting fully done

--- app/src/main/java/com/whatsapp/clone/presentation/settingDeleteMyAcc/DeleteMyAccActivity.java
+++ app/src/main/java/com/whatsapp/clone/presentation/settingDeleteMyAcc/DeleteMyAccActivity.java
@@ -20,9 +20,10 @@ import com.whatsapp.clone.widgets.settingDltAccNumber.SettingDltAccNumber;
 public class DeleteMyAccActivity extends AppCompatActivity {
     ActionBar actionBar;
     SettingDltAccNumber mSettingDltAccNumber;
-    SettingDeleteAccItem mSettingDeleteAccOne,mSettingDeleteAccTwo,mSettingDeleteAccThree,mSettingDeleteAccFour;
+    SettingDeleteAccItem mSettingDeleteAccOne, mSettingDeleteAccTwo, mSettingDeleteAccThree, mSettingDeleteAccFour;
     SettingChangeNumDeleteAcc mSettingChangeNumDeleteAcc;
     DeleteMyAcc mDeleteMyAcc;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,29 +34,32 @@ public class DeleteMyAccActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        mSettingDltAccNumber=findViewById(R.id.mDelete);
-        mSettingDeleteAccOne=findViewById(R.id.lineOne);
-        mSettingDeleteAccTwo=findViewById(R.id.lineTwo);
-        mSettingDeleteAccThree=findViewById(R.id.lineThree);
-        mSettingDeleteAccFour=findViewById(R.id.lineFour);
-        mSettingChangeNumDeleteAcc=findViewById(R.id.mChangeNum);
-        mDeleteMyAcc=findViewById(R.id.mDeleteAcc);
+        mSettingDltAccNumber = findViewById(R.id.mDelete);
+        mSettingDeleteAccOne = findViewById(R.id.lineOne);
+        mSettingDeleteAccTwo = findViewById(R.id.lineTwo);
+        mSettingDeleteAccThree = findViewById(R.id.lineThree);
+        mSettingDeleteAccFour = findViewById(R.id.lineFour);
+        mSettingChangeNumDeleteAcc = findViewById(R.id.mChangeNum);
+        mDeleteMyAcc = findViewById(R.id.mDeleteAcc);
     }
-    private void initWidget(){
+
+    private void initWidget() {
         mSettingDeleteAccOne.setTitle("Delete your account form WhatsApp");
         mSettingDeleteAccTwo.setTitle("Erase your message history");
         mSettingDeleteAccThree.setTitle("Delete you from all of your WhatsApp groups");
         mSettingDeleteAccFour.setTitle("Delete your payment history and cancel any pending payments");
     }
+
     private void onActionbarSetup() {
         actionBar = getSupportActionBar();
         ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#008069"));
         actionBar.setBackgroundDrawable(colorDrawable);
         actionBar.setTitle(R.string.titleDeleteMyAcc);
         actionBar.setDisplayHomeAsUpEnabled(true);
-        Window window= this.getWindow();
+        Window window = this.getWindow();
         window.setStatusBarColor(this.getResources().getColor(R.color.green));
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
